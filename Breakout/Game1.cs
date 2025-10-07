dd6f887a3214775676401ba323224a2129f78a25
Wojciech-Klis <14KlisW@student.smchull.org>
2025-10-07 21:34:18 +0100

    bricks disappear

--- Breakout/Game1.cs
+++ Breakout/Game1.cs
@@ -8,12 +8,11 @@ namespace Breakout
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-        private Sprite _paddle, _ball, _brick;
-        private Sprite[,] _bricks = new Sprite[5, 5];
+        private Sprite _paddle, _ball;
+        private Sprite[,] _bricks = new Sprite[6, 21];
         private bool _ballRight, _ballLeft = true, _ballUp, _ballDown = true;
         private Texture2D _paddleTexture, _ballTexture, _brickTexture;
         private Vector2 _paddlePos, _ballPos;
-        private float _ballVelocity;
         private Rectangle _ballBox, _paddleBox;
 
         public Game1()
@@ -51,6 +50,31 @@ namespace Breakout
                 _graphics.PreferredBackBufferHeight / 2 - _ballTexture.Height / 2);
             _ball = new Sprite(_ballTexture, _ballPos,
                 6.0f, Color.White);
+
+            int _gap = 11;
+            int _brickWidth = _brickTexture.Width;
+            int _brickHeight = _brickTexture.Height;
+            int _screenWidth = _graphics.PreferredBackBufferWidth;
+            int _screenHeight = _graphics.PreferredBackBufferHeight;
+            int _cols = 21;
+            int _rows = 6;
+            int _totalWidth = _cols * _brickWidth + (_cols - 1) * _gap;
+            int _startX = (_screenWidth - _totalWidth) / 2;
+            int _startY = 10; 
+
+            _bricks = new Sprite[_rows, _cols];
+
+            // create the brick grid
+            for (int _row = 0; _row < _rows; _row++)
+            {
+                for (int _col = 0; _col < _cols; _col++)
+                {
+                    float x = _startX + _col * (_brickWidth + _gap);
+                    float y = _startY + _row * (_brickHeight + _gap);
+
+                    _bricks[_row, _col] = new Sprite(_brickTexture, new Vector2(x, y), 0f, Color.White);
+                }
+            }
         }
 
         protected override void Update(GameTime gameTime)
@@ -140,6 +164,30 @@ namespace Breakout
                 _ball.SetPosition(_ballPos);
             }
 
+            for (int _row = 0; _row < _bricks.GetLength(0); _row++)
+            {
+                for (int _col = 0; _col < _bricks.GetLength(1); _col++)
+                {
+                    Sprite _brick = _bricks[_row, _col];
+                    if (_brick == null) continue; // skip destroyed bricks
+
+                    _brick.SetBoundingBox();
+
+                    Rectangle _brickBox = _brick.GetBoundingBox();
+
+                    if (_ballBox.Intersects(_brickBox))
+                    {
+                        _bricks[_row, _col] = null;
+                        _ball.GetPosition();
+                        _ballUp = false;
+                        _ballDown = true;
+                        _ball.SetPosition(_ballPos);
+                        _row = _bricks.GetLength(0);
+                        break;
+                    }
+                }
+            }
+
             base.Update(gameTime);
         }
 
@@ -152,9 +200,12 @@ namespace Breakout
             _spriteBatch.Begin();
             _paddle.DrawSprite(_spriteBatch);
             _ball.DrawSprite(_spriteBatch);
-            foreach (_brick in _bricks)
+            for (int row = 0; row < _bricks.GetLength(0); row++)
             {
-                _brick.DrawSprite(_spriteBatch);
+                for (int col = 0; col < _bricks.GetLength(1); col++)
+                {
+                    _bricks[row, col]?.DrawSprite(_spriteBatch);
+                }
             }
             _spriteBatch.End();
 
