fbcf680790cc9a2d67f162dbbb7cc5a064926d90
Wojciech-Klis <14KlisW@student.smchull.org>
2025-10-09 14:34:34 +0100

    jerzy tomaszewski

--- Breakout/Game1.cs
+++ Breakout/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace Breakout
 {
@@ -8,12 +9,16 @@ namespace Breakout
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-        private Sprite _paddle, _ball;
-        private Sprite[,] _bricks = new Sprite[6, 21];
-        private bool _ballRight, _ballLeft = true, _ballUp, _ballDown = true;
-        private Texture2D _paddleTexture, _ballTexture, _brickTexture;
-        private Vector2 _paddlePos, _ballPos;
+        private SpriteFont _gameOverText;
+        private Sprite _paddle, _ball, _jerzy;
+        private Sprite[,] _bricks = new Sprite[5,11];
+        private bool _ballRight, _ballLeft, _ballUp, _ballDown = true, _gameOver = false;
+        private int _lives = 3;
+        string _winText = $"Game Over!";
+        private Texture2D _paddleTexture, _ballTexture, _brickTexture, _gameOverScreen;
+        private Vector2 _paddlePos, _ballPos, _gameOverTextPos;
         private Rectangle _ballBox, _paddleBox;
+        Random _random = new Random();
 
         public Game1()
         {
@@ -40,23 +45,30 @@ namespace Breakout
             _paddleTexture = Content.Load<Texture2D>("paddle");
             _ballTexture = Content.Load<Texture2D>("ball");
             _brickTexture = Content.Load<Texture2D>("brick");
+            _gameOverText = Content.Load<SpriteFont>("gameOver");
+            _gameOverScreen = Content.Load<Texture2D>("jerzy tomaszewski");
 
             _paddlePos = new Vector2(_graphics.PreferredBackBufferWidth / 2 - _paddleTexture.Width / 2,
                 _graphics.PreferredBackBufferHeight - _paddleTexture.Height);
             _paddle = new Sprite(_paddleTexture, _paddlePos,
-                8.0f, Color.White);
+                12.0f, Color.White);
 
             _ballPos = new Vector2(_graphics.PreferredBackBufferWidth / 2 - _ballTexture.Width / 2,
                 _graphics.PreferredBackBufferHeight / 2 - _ballTexture.Height / 2);
             _ball = new Sprite(_ballTexture, _ballPos,
-                6.0f, Color.White);
+                8.0f, Color.White);
 
-            int _gap = 11;
+            Vector2 textSizeWin = _gameOverText.MeasureString(_winText);
+            _gameOverTextPos = new Vector2((_graphics.PreferredBackBufferWidth / 2f) - (textSizeWin.X / 2f),
+                (_graphics.PreferredBackBufferHeight / 2f) - (textSizeWin.Y / 2f));
+
+
+            int _gap = 10;
             int _brickWidth = _brickTexture.Width;
             int _brickHeight = _brickTexture.Height;
             int _screenWidth = _graphics.PreferredBackBufferWidth;
             int _screenHeight = _graphics.PreferredBackBufferHeight;
-            int _cols = 21;
+            int _cols = 11;
             int _rows = 6;
             int _totalWidth = _cols * _brickWidth + (_cols - 1) * _gap;
             int _startX = (_screenWidth - _totalWidth) / 2;
@@ -125,8 +137,18 @@ namespace Breakout
 
             if (_ballPos.Y >= _graphics.PreferredBackBufferHeight - _ballTexture.Height)
             {
-                _ballUp = true;
-                _ballDown = false;
+                _lives -= 1;
+                _ball.GetPosition();
+                _ballPos.X = (float)(_graphics.PreferredBackBufferWidth / 2) - (_ballTexture.Width / 2);
+                _ballPos.Y = (float)(_graphics.PreferredBackBufferHeight / 2) - (_ballTexture.Height / 2);
+                _ballLeft = false;
+                _ballRight = false;
+                _ball.SetPosition(_ballPos);
+
+                if (_lives < 1)
+                {
+                    _gameOver = true;
+                }
             }
 
             if (_ballBox.Intersects(_paddleBox))
@@ -138,7 +160,7 @@ namespace Breakout
             if (_ballDown == true)
             {
                 _ballPos = _ball.GetPosition();
-                _ballPos.Y += _ball.GetVelocity();
+                _ballPos.Y += _paddle.GetVelocity();
                 _ball.SetPosition(_ballPos);
 
             }
@@ -146,21 +168,21 @@ namespace Breakout
             if (_ballUp == true)
             {
                 _ballPos = _ball.GetPosition();
-                _ballPos.Y -= _ball.GetVelocity();
+                _ballPos.Y -= _paddle.GetVelocity();
                 _ball.SetPosition(_ballPos);
             }
 
             if (_ballLeft == true)
             {
                 _ballPos = _ball.GetPosition();
-                _ballPos.X -= _paddle.GetVelocity();
+                _ballPos.X -= _ball.GetVelocity();
                 _ball.SetPosition(_ballPos);
             }
 
             if (_ballRight == true)
             {
                 _ballPos = _ball.GetPosition();
-                _ballPos.X += _paddle.GetVelocity();
+                _ballPos.X += _ball.GetVelocity();
                 _ball.SetPosition(_ballPos);
             }
 
@@ -169,7 +191,7 @@ namespace Breakout
                 for (int _col = 0; _col < _bricks.GetLength(1); _col++)
                 {
                     Sprite _brick = _bricks[_row, _col];
-                    if (_brick == null) continue; // skip destroyed bricks
+                    if (_brick == null) continue;
 
                     _brick.SetBoundingBox();
 
@@ -188,25 +210,51 @@ namespace Breakout
                 }
             }
 
+            if (_ballBox.Intersects(_paddleBox) && _ballLeft == false && _ballRight == false)
+            {
+                int _randomDirection = _random.Next(1, 3);
+
+                if (_randomDirection == 1)
+                {
+                    _ballLeft = true;
+                }
+                if (_randomDirection == 2)
+                {
+                    _ballRight = true;
+                }
+            }
+
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.IndianRed);
 
             // TODO: Add your drawing code here
             
             _spriteBatch.Begin();
-            _paddle.DrawSprite(_spriteBatch);
-            _ball.DrawSprite(_spriteBatch);
-            for (int row = 0; row < _bricks.GetLength(0); row++)
+
+            if (_gameOver == false)
             {
-                for (int col = 0; col < _bricks.GetLength(1); col++)
+                _spriteBatch.Draw(_gameOverScreen, new Vector2(0, 0), Color.White);
+                _paddle.DrawSprite(_spriteBatch);
+                _ball.DrawSprite(_spriteBatch);
+                for (int _row = 0; _row < _bricks.GetLength(0); _row++)
                 {
-                    _bricks[row, col]?.DrawSprite(_spriteBatch);
+                    for (int _col = 0; _col < _bricks.GetLength(1); _col++)
+                    {
+                        _bricks[_row, _col]?.DrawSprite(_spriteBatch);
+                    }
                 }
             }
+
+            if (_gameOver == true)
+            {
+                GraphicsDevice.Clear(Color.Black);
+                _spriteBatch.DrawString(_gameOverText, _winText, _gameOverTextPos, Color.White);
+            }
+
             _spriteBatch.End();
 
             base.Draw(gameTime);
